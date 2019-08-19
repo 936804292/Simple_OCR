5e1ecbba681d139a66efd69fdcb5dd4bbb378bd6
936804292@qq.com <936804292@qq.com>
2019-08-19 18:10:22 +0800

    comfirm NO_progressImage

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from PIL import Image
 import cv2 as cv
 
 
-img = cv.imread("./code.jpeg")
+img = cv.imread("./123.jpg")
 gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 # thresh = cv.adaptiveThreshold(gray_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
@@ -22,9 +22,11 @@ img_bilater = cv.bilateralFilter(img,9,75,75)
 
 code = pytesseract.image_to_string(th)
 _mean = pytesseract.image_to_string(img_bilater)
+gray_s = pytesseract.image_to_string(gray_img)
 
-cv.imshow("_mean",img_mean)
-cv.imshow("thresh",th)
-print(code)
-print(_mean)
+
+#cv.imshow("_mean",img_mean)
+cv.imshow("gray_s",gray_img)
+print(gray_s)
+#print(_mean)
 cv.waitKey(0)
